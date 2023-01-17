a6e08f45ec362ec42b4c9f638fa3fce6cba11e00
Mitch Masia <mjmasia@gmail.com>
2023-01-16 22:35:06 -0600

    fix(tests): ensures proper auth during tests

--- lib/loin_web/components/header_components.ex
+++ lib/loin_web/components/header_components.ex
@@ -5,6 +5,17 @@ defmodule LoinWeb.HeaderComponents do
   use LoinWeb, :live_component
 
   @doc """
+  Not sure why we need this, CoreComponents doesn't have it.
+
+  The compiler is complaining and I don't have time to deal with it, so tossing it here.
+  """
+  def render(assigns) do
+    ~H"""
+    THIS IS A COMPILER PLACERHOLDER. DO NOT USE ME.
+    """
+  end
+
+  @doc """
   Renders an unauthenticated header.
   """
   attr :id, :string, required: true
