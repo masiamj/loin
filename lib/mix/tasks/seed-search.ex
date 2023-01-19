7d3895b564c66b1ca4a8009ca4a0997f9db40c88
Mitch Masia <mjmasia@gmail.com>
2023-01-19 01:04:52 -0600

    refactor(search): adds task to seed search file

--- lib/mix/tasks/seed-search.ex
+++ lib/mix/tasks/seed-search.ex
@@ -1,21 +1,22 @@
-defmodule Loin.FMP.Profiles do
-  @moduledoc """
-  Tasks for dealing with profiles.
-  """
-
+defmodule Mix.Tasks.Loin.Seed.Search do
+  @moduledoc "This task seeds the client-side search file."
   require Logger
+
+  use Mix.Task
 
   @doc """
   Seeds the profiles into a file.
   """
-  def seed(limit) when is_integer(limit) do
+  def run(_args) do
+    Mix.Task.run("app.start")
+
     profiles =
       Loin.FMP.Service.all_profiles_stream()
       |> Stream.map(fn item ->
         Logger.info("Processing #{item.symbol}")
         Map.take(item, [:image, :industry, :market_cap, :name, :sector, :symbol])
       end)
-      |> Enum.take(limit)
+      |> Enum.to_list()
       |> Jason.encode!()
 
     File.write("./assets/js/hooks/StockSearcher/stock-profiles.json", profiles, [:binary])
