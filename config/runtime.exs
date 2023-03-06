0f1e7e651ddf5c2ac693e3ff5f268f60be7ed23b
Mitch Masia <mjmasia@gmail.com>
2023-03-06 17:57:16 -0600

    feat(host): updates to trendflares.com

--- config/runtime.exs
+++ config/runtime.exs
@@ -49,11 +49,11 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "trendflares.com"
+  # host = System.get_env("PHX_HOST") || "trendflares.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :loin, LoinWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: "trendflares.com", port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
